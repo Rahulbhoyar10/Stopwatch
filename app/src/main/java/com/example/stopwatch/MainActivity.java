9cb81d10b0fb5386487bab1f286e3ae882412f7e
rahulbhoyar10 <rahulbhoyar1995@gmail.com>
2024-01-13 12:50:35 +0530

    final commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Chronometer chronometer;
+    private ImageButton btnReset, btnLap, setting;
+    private ListView lapListView;
+    private ToggleButton btnStartPause;
+    private boolean isRunning = false;
+    private long startTime, elapsedTime;
+
+    private final Handler handler = new Handler();
+    private ArrayList<String> lapList;
+    private ArrayAdapter<String> lapAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        btnStartPause = findViewById(R.id.btnStartPause);
+        btnReset = findViewById(R.id.btnReset);
+        btnLap = findViewById(R.id.btnLap);
+        lapListView = findViewById(R.id.laplistView);
+        setting = findViewById(R.id.setting);
+
+        lapList = new ArrayList<>();
+        lapAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lapList);
+        lapListView.setAdapter(lapAdapter);
+
+        btnStartPause.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                isRunning = true;
+                startTime = SystemClock.elapsedRealtime() - elapsedTime;
+                handler.postDelayed(updateTimer, 0);
+            } else {
+                isRunning = false;
+                elapsedTime = SystemClock.elapsedRealtime() - startTime;
+                handler.removeCallbacks(updateTimer);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRunning = false;
+                btnStartPause.setChecked(false);
+                elapsedTime = 0;
+                handler.removeCallbacks(updateTimer);
+                updateUI();
+                lapList.clear();
+                lapAdapter.notifyDataSetChanged();
+            }
+        });
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Nothing Here", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    String lapTime = chronometer.getText().toString();
+                    lapList.add("Lap " + lapList.size() + ": " + lapTime);
+                    lapAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    private final Runnable updateTimer = new Runnable() {
+        @Override
+        public void run() {
+            if (isRunning) {
+                elapsedTime = SystemClock.elapsedRealtime() - startTime;
+                updateUI();
+                handler.postDelayed(this, 30); // Update every 30 milliseconds
+            }
+        }
+    };
+
+    private void updateUI() {
+        int minutes = (int) (elapsedTime / 60000);
+        int seconds = (int) (elapsedTime % 60000) / 1000;
+        int milliseconds = (int) (elapsedTime % 1000);
+
+        String timerText = String.format("%02d:%02d:%03d", minutes, seconds, milliseconds);
+        chronometer.setText(timerText);
+    }
+}
+
